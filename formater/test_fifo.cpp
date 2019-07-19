83a6699264d06e25b6767f2385a6ea0b79a8d4db
Mathieu Kapfer <mathieu.kapfer@gmail.com>
2019-07-19 13:32:22 +0200

    [fifo] unit test

--- formater/test_fifo.cpp
+++ formater/test_fifo.cpp
@@ -1,6 +1,14 @@
 #include "LogFifo.h"
 #include <stdio.h>
 
+#define DUMP(cmd, ...)                                                  \
+  {                                                                     \
+    char _cmd[10];                                                      \
+    snprintf(_cmd, 10, cmd, ##__VA_ARGS__ ) ;                           \
+    printf("%-10s, front:%-20s back:%-20s, empty:%d, full:%d\n",        \
+           _cmd, fifo.front(), fifo.back(), fifo.isEmpty(), fifo.isFull() ); \
+  }
+
 
 int main(int argc, char *argv[]) {
 
@@ -8,38 +16,38 @@ int main(int argc, char *argv[]) {
   LogFifo fifo(dummyMutex);
 
   // normal usage
+  DUMP("init");
   fifo.push("this is a log 1");
-  printf("push:front:%s (back:%s)\n", fifo.front(), fifo.back());
+  DUMP("push");
   fifo.push("this is a log 2222");
-  printf("push:front:%s (back:%s)\n", fifo.front(), fifo.back());
+  DUMP("push");
   fifo.push("this is a log 3333333");
-  printf("push:front:%s (back:%s)\n", fifo.front(), fifo.back());
+  DUMP("push");
   fifo.push("this is a log 4444444444");
-  printf("front-same:%s (back:%s)\n", fifo.front(), fifo.back());
-  printf("front-same:%s (back:%s)\n", fifo.front(), fifo.back());
-  printf("front-same:%s (back:%s)\n", fifo.front(), fifo.back());
+  DUMP("nothing");
+  DUMP("nothing");
+  DUMP("nothing");
   fifo.pop();
-  printf("pop:front:%s (back:%s)\n", fifo.front(), fifo.back());
+  DUMP("pop");
   fifo.pop();
-  printf("pop:front:%s (back:%s)\n", fifo.front(), fifo.back());
+  DUMP("pop");
   fifo.pop();
-  printf("pop:front:%s (back:%s)\n", fifo.front(), fifo.back());
+  DUMP("pop");
   fifo.pop();
-  printf("pop:front:%s (back:%s)\n", fifo.front(), fifo.back());
 
   // too many push
   for (int i=0; i < 40; i++) {
     char buf[100];
     snprintf(buf, 100, "too many push %d", i);
     fifo.push(buf);
-    printf("push[%d]:front:%s (back:%s)\n", i, fifo.front(), fifo.back());
+    DUMP("push %d", i);
   }
 
   // too many pop
   for (int i=0; i < 50; i++) {
     char buf[100];
-    printf("pop[%d]:front:%s (back:%s)\n ", i, fifo.front(), fifo.back());
     fifo.pop();
+    DUMP("pop %d", i);
   }
 
   return 0;
