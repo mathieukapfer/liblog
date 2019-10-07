36b927e3e02eb72df66b261f647bb947cf36276c
Mathieu Kapfer <mathieu.kapfer@gmail.com>
2019-10-07 16:39:23 +0200

    Update tab show

--- log_formater.h
+++ log_formater.h
@@ -10,23 +10,27 @@
    NOTE: return the same pointer as buf, but filled with tab value as string
 */
 template <typename T>
-char * log_table2str(T tab[], int tabSize, char *buf, int bufSize) {
+char * log_table2str(T tab[], int nb, int start, char *buf, int bufSize) {
   int pos = 0;
   pos += snprintf(buf + pos, bufSize - pos, "{");
-  for (int i=0; i < tabSize; i++) {
+  for (int i=start; i < nb + start; i++) {
     pos += snprintf(buf + pos, bufSize - pos, "%d, ", tab[i]);
   }
   pos += snprintf(buf + pos, bufSize - pos, "}");
   return buf;
 }
 
-template char * log_table2str(uint8_t tab[], int tabSize, char *buf, int bufSize);
-template char * log_table2str(uint16_t tab[], int tabSize, char *buf, int bufSize);
-template char * log_table2str(uint32_t tab[], int tabSize, char *buf, int bufSize);
-template char * log_table2str(uint64_t tab[], int tabSize, char *buf, int bufSize);
+template char * log_table2str(uint8_t tab[], int tabSize, int start,char *buf, int bufSize);
+template char * log_table2str(uint16_t tab[], int tabSize, int start,char *buf, int bufSize);
+template char * log_table2str(uint32_t tab[], int tabSize, int start,char *buf, int bufSize);
+template char * log_table2str(uint64_t tab[], int tabSize, int start,char *buf, int bufSize);
+template char * log_table2str(int8_t tab[], int tabSize, int start,char *buf, int bufSize);
+template char * log_table2str(int16_t tab[], int tabSize, int start,char *buf, int bufSize);
+template char * log_table2str(int32_t tab[], int tabSize, int start,char *buf, int bufSize);
+template char * log_table2str(int64_t tab[], int tabSize, int start,char *buf, int bufSize);
 
 /* macro to use the formater */
-#define LOG_TABLE_TO_STR(tab, size)                  \
-  log_table2str(tab, size, logBuf, logBufSize)
+#define LOG_TABLE_TO_STR(tab, nb, start)              \
+  log_table2str(tab, nb, start, logBuf, logBufSize)
 
 #endif /* LOG_FORMATER_H */
