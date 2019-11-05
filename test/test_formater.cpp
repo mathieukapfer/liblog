9ab619a50545da29a29a0e682269e4210019eeaf
Mathieu Kapfer <mathieu.kapfer@gmail.com>
2019-11-05 16:45:48 +0100

    [formater] fix [u]int64

--- test/test_formater.cpp
+++ test/test_formater.cpp
@@ -16,23 +16,31 @@
 
 int main(int argc, char *argv[]) {
 
-  char buf[] = { 1, 2, 3, 4, 5, 6, 7, 8 };
+  char buf[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
   char logBuf[logBufSize];
 
-  // default format and start at index 0
-  printf("%s\n", LOG_TABLE_TO_STR(buf, 4, 0));
-  printf("%s\n", LOG_TABLE_TO_STR((uint16_t * )buf, 2, 0));
-  printf("%s\n", LOG_TABLE_TO_STR((uint32_t * )buf, 1, 0));
+  printf("\n default format and start at index 0\n");
+  printf("%s\n", LOG_TABLE_TO_STR(buf, 8, 0));
+  printf("%s\n", LOG_TABLE_TO_STR((uint16_t * )buf, 4, 0));
+  printf("%s\n", LOG_TABLE_TO_STR((uint32_t * )buf, 2, 0));
+  printf("%s\n", LOG_TABLE_TO_STR((uint64_t * )buf, 1, 0));
 
-  // use formater
-  printf("%s\n", LOG_TABLE_TO_STR_(buf, 4, 0, "0x%x, "));
-  printf("%s\n", LOG_TABLE_TO_STR_((uint16_t * )buf, 2, 0, "0x%x, "));
-  printf("%s\n", LOG_TABLE_TO_STR_((uint32_t * )buf, 1, 0, "0x%x, "));
+  printf("\n formater d");
+  printf("%s\n", LOG_TABLE_TO_STR_(buf, 8, 0, "%d, "));
+  printf("%s\n", LOG_TABLE_TO_STR_((uint16_t * )buf, 4, 0, "%d, "));
+  printf("%s\n", LOG_TABLE_TO_STR_((uint32_t * )buf, 2, 0, "%ld, "));
+  printf("%s\n", LOG_TABLE_TO_STR_((uint64_t * )buf, 1, 0, "%lld, "));
 
-  // start with 1
-  printf("%s\n", LOG_TABLE_TO_STR_(buf, 4, 1, "0x%x, "));
-  printf("%s\n", LOG_TABLE_TO_STR_((uint16_t * )buf, 2, 1, "0x%x, "));
-  printf("%s\n", LOG_TABLE_TO_STR_((uint32_t * )buf, 1, 1, "0x%x, "));
+  printf("\n formater x");
+  printf("%s\n", LOG_TABLE_TO_STR_(buf, 8, 0, "0x%x, "));
+  printf("%s\n", LOG_TABLE_TO_STR_((uint16_t * )buf, 4, 0, "0x%x, "));
+  printf("%s\n", LOG_TABLE_TO_STR_((uint32_t * )buf, 2, 0, "0x%lx, "));
+  printf("%s\n", LOG_TABLE_TO_STR_((uint64_t * )buf, 1, 0, "0x%llx, "));
+
+  printf("\n start with 4");
+  printf("%s\n", LOG_TABLE_TO_STR_(buf, 8, 4, "0x%x, "));
+  printf("%s\n", LOG_TABLE_TO_STR_((uint16_t * )buf, 4, 4, "0x%x, "));
+  printf("%s\n", LOG_TABLE_TO_STR_((uint32_t * )buf, 2, 4, "0x%x, "));
 
   return 0;
 }
