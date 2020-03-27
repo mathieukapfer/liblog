04a95aeeb2839de6f9e67daf219690a6e5b78f75
Mathieu Kapfer <mathieu.kapfer@gmail.com>
2020-03-27 16:59:37 +0100

    [fix] compile issue on m3 conf

--- liblogConfig.cmake
+++ liblogConfig.cmake
@@ -1,9 +1,10 @@
 
 message("*** Package found for " ${CMAKE_CURRENT_SOURCE_DIR})
 
-# include path
-set(log_INCLUDE_DIRS ${CMAKE_SOURCE_DIR}/lib/liblog )
-# archive path
-set(log_LIBRARIES ${CMAKE_BINARY_DIR}/lib/liblog/liblog.a )
 # compilation switch to use with
 include(${CMAKE_SOURCE_DIR}/lib/liblog/CMakeLists_LogConfig.txt)
+
+# include path
+set(log_INCLUDE_DIRS ${CMAKE_SOURCE_DIR}/lib/liblog ${CMAKE_SOURCE_DIR}/lib/liblog/src/fifo)
+# archive path
+set(log_LIBRARIES ${CMAKE_BINARY_DIR}/lib/liblog/liblog.a )
