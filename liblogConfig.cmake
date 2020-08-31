ca3bebe286247e2f33126026c27f043b6e09bb19
Mathieu Kapfer <mathieu.kapfer@gmail.com>
2020-08-31 17:34:04 +0200

    [root path] change path in root project (!)

--- liblogConfig.cmake
+++ liblogConfig.cmake
@@ -2,9 +2,9 @@
 message("*** Package found for " ${CMAKE_CURRENT_SOURCE_DIR})
 
 # compilation switch to use with
-include(${CMAKE_SOURCE_DIR}/lib/liblog/CMakeLists_LogConfig.txt)
+include(${CMAKE_SOURCE_DIR}/src/lib/liblog/CMakeLists_LogConfig.txt)
 
 # include path
-set(log_INCLUDE_DIRS ${CMAKE_SOURCE_DIR}/lib/liblog ${CMAKE_SOURCE_DIR}/lib/liblog/src/fifo)
+set(log_INCLUDE_DIRS ${CMAKE_SOURCE_DIR}/src/lib/liblog ${CMAKE_SOURCE_DIR}/src/lib/liblog/src/fifo)
 # archive path
-set(log_LIBRARIES ${CMAKE_BINARY_DIR}/lib/liblog/liblog.a )
+set(log_LIBRARIES ${CMAKE_BINARY_DIR}/src/lib/liblog/liblog.a )
