702ca126ff2a7b4c3bc0683795c6abe4959ba726
mathieu Kapfer <mathieu.kapfer@non.se.com>
2020-10-13 17:23:18 +0200

    [move] less files at root dir

--- liblogConfig.cmake
+++ liblogConfig.cmake
@@ -5,6 +5,6 @@ message("*** Package found for " ${CMAKE_CURRENT_SOURCE_DIR})
 include(${CMAKE_SOURCE_DIR}/src/lib/liblog/CMakeLists_LogConfig.txt)
 
 # include path
-set(log_INCLUDE_DIRS ${CMAKE_SOURCE_DIR}/src/lib/liblog ${CMAKE_SOURCE_DIR}/src/lib/liblog/src/fifo)
+set(log_INCLUDE_DIRS ${CMAKE_SOURCE_DIR}/src/lib/liblog ${CMAKE_SOURCE_DIR}/src/lib/liblog/src ${CMAKE_SOURCE_DIR}/src/lib/liblog/src/fifo)
 # archive path
 set(log_LIBRARIES ${CMAKE_BINARY_DIR}/src/lib/liblog/liblog.a )
